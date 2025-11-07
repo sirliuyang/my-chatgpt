af7188726d848851b2d5c142c3dea7d7bd40a83a
Leon <88978827@qq.com>
2025-11-07 23:02:02 +0800

    config port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    server: {
+        port: 9007, // 指定端口号
+        host: true, // 允许外部访问
+        open: true  // 自动打开浏览器
+    }
 })
